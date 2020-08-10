4d13824f1a47064c81d1a51e7ae74ac10fd2705d
Chris-Bortel <bortelcc19@gmail.com>
2020-08-09 22:05:34 -0700

    cli is taking an action and a value and prints a constructed object

---
+++ index.js
@@ -0,0 +1,13 @@
+// // takes in user input and spit out the action and the payload (note that they entered in)
+'use strict';
+
+const Input = require('./lib/input');
+
+const Notes = require('./lib/notes.js');
+
+const input = new Input();
+console.log(input);
+// const notes = new Notes(input);
+// const output = notes.go();
+
+// console.log(output);
