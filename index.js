7f6d0ef7d24a2acbf34753cb370eba49aa286d56
Chris-Bortel <bortelcc19@gmail.com>
2020-08-12 15:58:58 -0700

    action is being validated, working on now checking for corressponding data

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 'use strict';
 
 const Input = require('./lib/input');
-
 const Notes = require('./lib/notes.js');
 
 const input = new Input();
-
-
 const notes = new Notes(input);
-console.log(notes);
+
 
 // console.log(output);
