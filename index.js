4dd179ad92c710aaf9a78805bcaa5b03664c91cc
Chris-Bortel <bortelcc19@gmail.com>
2020-08-12 16:39:38 -0700

    I am now printing the validated note as a confirmation message, 'Adding Note: This is cool'

--- index.js
+++ index.js
@@ -6,5 +6,12 @@ const Notes = require('./lib/notes.js');
 const input = new Input();
 const notes = new Notes(input);
 
+input.validInput()?notes.execute(): help();
+
+function help() {
+  console.log('this is not valid');
+  process.exit();
+}
+
 
 // console.log(output);
