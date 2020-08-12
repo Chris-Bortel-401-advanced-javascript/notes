5ae52ff6c6c63626b59b38b08c3c1a300252f0a1
Chris-Bortel <bortelcc19@gmail.com>
2020-08-11 21:49:06 -0700

    Input.js is returning an instance containing the action to perform the payload

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const Input = require('./lib/input');
 const Notes = require('./lib/notes.js');
 
 const input = new Input();
-console.log(input);
 
-// const notes = new Notes(input);
-// console.log(notes);
+
+const notes = new Notes(input);
+console.log(notes);
 
 // console.log(output);
