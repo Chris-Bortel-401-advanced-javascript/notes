f60320fb1889aa56a2a6c3688d04630c8f58f989
Chris-Bortel <bortelcc19@gmail.com>
2020-08-14 16:12:41 -0700

    my input test is working. To get it to work, I had to put the mockImplementation above the where I required the input.js file.

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -1,11 +1,36 @@
 'use strict';
 
+jest.mock('minimist');
+const minimist = require('minimist');
+minimist.mockImplementation(() => {
+  return {
+    a: 'this is a new note',
+    add: 'this is a new note',
+  };
+});
+
+const Input = require('../lib/input');
+
 describe('Command Line Note Taker', () => {
 
+  // refers to what we are testing
   it('can take notes from my terminal', () => {
-
-    expect(true).toBeTruthy();
+    // instantiates new Input object for this test
+    const opts = new Input();
+    const command = opts.validate({a: 'this should pass'});
+    expect(command.action).toBeDefined();
+    expect(command.payload).toBeDefined();
+    // const options = new Input();
+    // console.log('this is options line 10++++++++++++++++++++++++++++++++', options);
+    // expect(options.validate()).toBeTruthy();
 
   });
-  
 });
+
+
+
+
+
+// instiation an instance of the mockImplementation(), which acts as a mock command line for the test
+
+
