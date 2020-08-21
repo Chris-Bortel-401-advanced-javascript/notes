1d82fd24810d6c1bfde9333fb3b7d52ceca7ca84
Chris-Bortel <bortelcc19@gmail.com>
2020-08-20 17:11:30 -0700

    wrote test for validate and it passes as expected

--- __tests__/input.test.js
+++ __tests__/input.test.js
@@ -4,38 +4,46 @@ jest.mock('minimist');
 
 const minimist = require('minimist');
 
-minimist.mockImplementation(() => {
-  return {
-    a: 'this is a new note',
-    add: 'this is a new note',
-  };
-});
-
 const Input = require('../lib/input.js');
 
 describe('Command Line Note Taker', () => {
   // refers to what we are testing
-  it('with a good input, validate() returns true and creates a new instance of both acton and payload', () => {
-    // instantiates new Input object for this test
-    const opts = new Input();
-    const command = opts.validate({a: 'this should pass'});
-    expect(command.action).toBeDefined();
-    expect(command.payload).toBeDefined();
+  it('with a good input, validate() returns true and creates a new instance of both action and payload', () => {
+    minimist.mockImplementation(() => {
+      return {
+        a: 'this is a new note',
+      };
+    });
 
+    const opts = new Input();
+    expect(opts.command.action).toBeDefined();
+    expect(opts.command.payload).toBeDefined();
   });
-//   Given invalid input:
-// The Class’ valid() method returns false
+
+  // The Class’ valid() method returns false
   it('returns undefined if the validate() method returns false', () => {
     minimist.mockImplementation(() => {
       return {
         wsd: 'not a note',
       };
     });
+
+    const opts = new Input();
+    console.log('opts line 32', opts);
+    expect(opts.command.action).toBeUndefined();
+    expect(opts.command.payload).toBeUndefined();
   });
 
-  it('returns true if validInput contains both a command and an action', () => {
+  it('returns true if the action and the payload exist', () => {
+    minimist.mockImplementation(() => {
+      return {
+        a: 'this is a valid input',
+      };
+    });
     
-  })
+  });
+
+
 });
 
 
